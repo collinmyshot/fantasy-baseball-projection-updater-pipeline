242fbae5f92db7ed1179d176f1de40fd05dad0d7
CK <collinisprettygreat@gmail.com>
2026-02-19 23:33:29 -0800

    Harden shinyapps deploy secret parsing and set explicit server

--- scripts/deploy_shinyapps.R
+++ scripts/deploy_shinyapps.R
@@ -4,13 +4,20 @@ suppressPackageStartupMessages({
   library(rsconnect)
 })
 
+normalize_secret <- function(x) {
+  x <- trimws(as.character(x))
+  x <- sub('^"(.*)"$', "\\1", x)
+  x <- sub("^'(.*)'$", "\\1", x)
+  x
+}
+
 args <- commandArgs(trailingOnly = TRUE)
 app_dir <- if (length(args) >= 1) args[[1]] else "."
 app_name <- if (length(args) >= 2) args[[2]] else Sys.getenv("SHINYAPP_NAME", "fantasy-baseball-projections")
 
-account <- Sys.getenv("SHINYAPPS_ACCOUNT", "")
-token <- Sys.getenv("SHINYAPPS_TOKEN", "")
-secret <- Sys.getenv("SHINYAPPS_SECRET", "")
+account <- normalize_secret(Sys.getenv("SHINYAPPS_ACCOUNT", ""))
+token <- normalize_secret(Sys.getenv("SHINYAPPS_TOKEN", ""))
+secret <- normalize_secret(Sys.getenv("SHINYAPPS_SECRET", ""))
 
 if (!nzchar(account) || !nzchar(token) || !nzchar(secret)) {
   stop(
@@ -28,13 +35,15 @@ message("Deploying app '", app_name, "' from ", normalizePath(app_dir, mustWork 
 rsconnect::setAccountInfo(
   name = account,
   token = token,
-  secret = secret
+  secret = secret,
+  server = "shinyapps.io"
 )
 
 rsconnect::deployApp(
   appDir = app_dir,
   appName = app_name,
   account = account,
+  server = "shinyapps.io",
   launch.browser = FALSE,
   forceUpdate = TRUE
 )
